e21202ee5b3ea03ca7174cfd9e9de1e3e9e0d6d8
phamminhtu21 <pmtu.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-09 22:55:18 +0700

    thêm file

---
+++ 5.2.py
@@ -0,0 +1,12 @@
+#Chuong trinh toan hoc  
+#Tinh toan so hoc
+x=10
+y=5
+tong=x+y
+hieu=x-y
+tich=x*y
+thuong=x/y
+print('Tong cua hai so ', x,'+', y,'=,tong')
+print('Hieu cua hai so ', x,'-', y, '=',hieu)
+print('Tich cua hai so ', x,'*', y, '=',tich)
+print('Thuong cua hai so ', x,'/', y, '=',thuong)
