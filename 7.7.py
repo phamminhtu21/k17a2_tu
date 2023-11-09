25c39cd18ffe3c6e037a40d87d9245fb5019fcea
phamminhtu21 <pmtu.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-09 23:48:35 +0700

    thêm file

---
+++ 7.7.py
@@ -0,0 +1,9 @@
+so_tien_muon_doi = 1375000
+so_to_1 = so_tien_muon_doi // 500000
+tien_con_lai = so_tien_muon_doi % 500000
+so_to_2 = tien_con_lai // 200000
+tien_con_lai = tien_con_lai % 200000
+so_to_3 = tien_con_lai // 100000
+tien_con_lai = tien_con_lai % 100000
+so_to_4 = tien_con_lai // 50000
+print('doi tien 1375000=', so_to_1, so_to_2, so_to_3, so_to_4)
