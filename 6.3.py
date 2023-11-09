daf0b03a705848f9b2fa079f1e5f7d66e555b406
phamminhtu21 <pmtu.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-09 23:10:19 +0700

    thêm file

---
+++ 6.3.py
@@ -0,0 +1,5 @@
+ten_hang=int(input("Sua hop Vina Milk"))
+so_luong=int(input("so luong"))
+don_gia=int(input("don gia"))
+tien_phai_tra=so_luong*don_gia
+print("tien_phai_tra=",so_luong,"=",don_gia,"=",tien_phai_tra) 
